0d03e1c1743e8deb03985b8c9a73a831d8aa5f54
Helmut Chlebecek <helmutchlebecek@Helmut-Chlebeceks-MacBook-Pro.local>
2011-09-21 08:40:58 +0200

    fixed buts

--- app/models/auto.rb
+++ app/models/auto.rb
@@ -1,7 +1,7 @@
 
 
 class Auto < ActiveRecord::Base
-  attr_accessible :hersteller, :modell, :land, :baujahr, :typ, :datenbanknr, :datenbanknralt, :status, :farbe, :antrieb, :konstruktion, :raeder, :ausstattung, :abmessungl, :abmessungb, :abmessungh, :gewichtinkg, :relevanz, :geschaetztermarktwert, :sonstiges, :anmerkungen, :kaufdatum, :ebaybenutzername, :verkaeufer, :ebayartikelnummer, :modellbeschreibung, :farbekauf, :anmerkungnachkauf, :zahlungsart, :zahlungsdatum, :waehrung, :kaufpreis, :transport, :summe, :ersatzteile, :restaurier, :gesamt, :assets_attributes
+  attr_accessible :hersteller, :modell, :land, :baujahr, :typ, :datenbanknr, :datenbanknralt, :status, :farbe, :antrieb, :konstruktion, :raeder, :ausstattung, :abmessungl, :abmessungb, :abmessungh, :gewichtinkg, :relevanz, :geschaetztermarktwert, :sonstiges, :anmerkungen, :kaufdatum, :ebaybenutzername, :verkaeufer, :ebayartikelnummer, :modellbeschreibung, :farbekauf, :anmerkungnachkauf, :zahlungsart, :zahlungsdatum, :waehrung, :kaufpreis, :transport, :summe, :ersatzteile, :restaurier, :gesamt, :assets_attributes, :fw, :fwkaufpreis, :fwtransport, :fwsumme, :fwersatzteile, :fwrestaurier, :fwgesamt
   has_many :assets
   accepts_nested_attributes_for :assets, :allow_destroy => true
   
